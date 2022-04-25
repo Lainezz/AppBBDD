0c60345bb3f1bba99a3e5f6a2d98f6c3065c4b08
Lainezz <diegolinaresmojacar@gmail.com>
2022-04-25 13:31:28 +0200

    Adicion del metodo conectar Algo más que escribo No sé qué más poner

--- src/com/conexionMySql/ConexionMySql.java
+++ src/com/conexionMySql/ConexionMySql.java
@@ -1,5 +1,31 @@
 package com.conexionMySql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 public class ConexionMySql {
 
+	
+	public String db = "notarbd";
+    public String url = "jdbc:mysql://localhost/"+db;
+    public String user = "root";
+    public String pass = "";
+	
+	public Connection conectar() {
+		Connection con = null;
+
+        try{
+            //cargamos el Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // creamos un enlace
+            con = DriverManager.getConnection(this.url, this.user, this.pass);
+        }
+        catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return con;
+	}
 }
