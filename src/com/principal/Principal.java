d53dcf0f72df7aaee3f5d0f7de0090c1ebd5b48f
Lainezz <diegolinaresmojacar@gmail.com>
2022-05-12 12:52:17 +0200

    prueba

--- src/com/principal/Principal.java
+++ src/com/principal/Principal.java
@@ -5,6 +5,7 @@ import com.conexionMySql.ConexionMySql;
 public class Principal {
 
 	public static void main(String[] args) {
+		//Prueba de comentario
 		ConexionMySql.getInstance().conectar();
 		ConexionMySql.getInstance().desconectar();
 
