32d5061c4df7a87d979722430739826112ae2023
Lainezz <diegolinaresmojacar@gmail.com>
2022-05-12 12:53:24 +0200

    prueba de conflicto

--- src/com/principal/Principal.java
+++ src/com/principal/Principal.java
@@ -5,6 +5,7 @@ import com.conexionMySql.ConexionMySql;
 public class Principal {
 
 	public static void main(String[] args) {
+		//Otra prueba
 		ConexionMySql.getInstance().conectar();
 		ConexionMySql.getInstance().desconectar();
 
